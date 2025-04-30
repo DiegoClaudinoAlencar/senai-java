78c111b4a7b467afa5fa82899755e525e1768186
DiegoClaudinoAlencar <di.baixista@hotmail.com>
2025-04-29 21:48:14 -0300

    feat: serviços

--- src/main/java/com/escola/senai/service/AlunoService.java
+++ src/main/java/com/escola/senai/service/AlunoService.java
@@ -1,4 +1,14 @@
 package com.escola.senai.service;
 
+import com.escola.senai.Interface.Alunorepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlunoService {
+
+    private final Alunorepository alunorepository;
+
+    public AlunoService(Alunorepository alunorepository) {
+        this.alunorepository = alunorepository;
+    }
 }
