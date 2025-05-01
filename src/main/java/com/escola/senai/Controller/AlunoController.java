7b69c15988c4c06d03eab856fe4d59b8e84874c0
DiegoClaudinoAlencar <di.baixista@hotmail.com>
2025-04-30 21:54:30 -0300

    Controller

--- src/main/java/com/escola/senai/Controller/AlunoController.java
+++ src/main/java/com/escola/senai/Controller/AlunoController.java
@@ -1,4 +1,56 @@
 package com.escola.senai.Controller;
 
+import com.escola.senai.model.Aluno;
+import com.escola.senai.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@@RequestMapping("/alunos")
 public class AlunoController {
+
+    private final AlunoService service;
+
+    public AlunoController(AlunoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Aluno> listaAlunos (){
+        return service.listaDeTodosAlunos();
+    }
+
+    @PostMapping
+    public Aluno cadastrarAluno(@RequestBody Aluno aluno){
+        return service.salvaNovAluno(aluno);
+    }
+
+    @GetMapping("/{id}")
+    public Aluno buscarAlunoPorId(@PathVariable long id){
+        return service.buscarAlunoPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Aluno atualizarAluno(@PathVariable long id, RequestBody Aluno aluno){
+        Aluno existe = service.buscarAlunoPorId(id);
+        return  service.buscarAlunoPorId(id)
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarAluno (@PathVariable long id){
+        service.excluirAluno(id);
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
+
--- src/main/java/com/escola/senai/service/AlunoService.java
+++ src/main/java/com/escola/senai/service/AlunoService.java
@@ -1,7 +1,13 @@
 package com.escola.senai.service;
 
+
 import com.escola.senai.Interface.Alunorepository;
+import com.escola.senai.model.Aluno;
+import com.fasterxml.jackson.databind.util.ArrayBuilders;
+import org.hibernate.mapping.List;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 public class AlunoService {
@@ -11,4 +17,25 @@ public class AlunoService {
     public AlunoService(Alunorepository alunorepository) {
         this.alunorepository = alunorepository;
     }
+
+    public List<Aluno> listaDeTodosAlunos (){
+        return (List<Aluno>) alunorepository.findAll();
+    }
+
+    public Aluno salvaNovAluno (Aluno aluno){
+        return alunorepository.save(aluno);
+    }
+
+
+    public Aluno buscarAlunoPorId(Long id) {
+        return alunorepository.findById(id).orElse(null);
+    }
+
+    public void excluirAluno(Long id ){
+        alunorepository.deleteById((id));
+    }
+
+
+
+
 }
